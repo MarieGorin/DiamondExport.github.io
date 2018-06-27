981912643bfadcd271d23520c1efad4ecb365da0
LinaThn <38690854+LinaThn@users.noreply.github.com>
2018-06-27 13:26:27 +0200

    index js

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ MongoClient.connect(url, function(err, db) {
     var dbo = db.db("diamond");
 
     const app = express();
-    app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+    app.engine('handlebars', exphbs({defaultLayout: 'main'; extname:'hbs'; layoutsDir: __DiamondExport + '/views/layout'}));
     app.set('view engine', 'handlebars');
 
     // parse application/x-www-form-urlencoded
@@ -24,10 +24,29 @@ MongoClient.connect(url, function(err, db) {
     app.use(express.static('client'));
 
     app.post('/post-feedback', function (req, res) {
-        db.collection('feedbacks').insertOne(req.body);    
-        res.redirect('/');
+        var feedback = req.body.feedback;
+    var collection = req.db.get('diamond');
+    collection.insert({"diamond": feedback}, function (err, doc) {
+        if (err) {
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            res.redirect("/client/message")
+        }
     });
+});
 
+    app.get('/client/message', function(req, res) {
+        var collection = req.db.get('diamond');
+        collection.find({}, {}, function(err, docs) {
+           var obj = {
+             fromDB: docs,
+             title: "Messages"
+           }
+           res.render('post-feedback', obj);
+        });
+    });
+    
     app.listen(2179, () => console.log('App listening on port 2179!'));
 });
 
